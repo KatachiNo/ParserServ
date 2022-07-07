81dfe5c20944f1c59553829e283dbc63da51c72a
Никита Зимин <m1900757@edu.misis.ru>
2022-07-07 23:04:52 +0300

    Добавил возможность взаимодействия с бд

---
+++ ParserServ/SqlAccess/SqlCrud.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserServ.SqlAccess
+{
+    internal class SqlCrud
+    {
+        private readonly string _connectionString;
+        private SqliteDataAccess _dataAccess;
+        public SqlCrud()
+        {
+            _dataAccess = new SqliteDataAccess();
+            _connectionString = ConfigurationManager.ConnectionStrings["XmlPath"].ConnectionString;
+        }
+
+    }
+}
