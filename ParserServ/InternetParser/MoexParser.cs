e782008cfaa4e188ad6ad8dbb1db762fe612a603
Никита Зимин <m1900757@edu.misis.ru>
2022-07-08 12:52:26 +0300

    добавил запрос к бд

---
+++ ParserServ/InternetParser/MoexParser.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlAccess;
+
+namespace ParserServ.InternetParser
+{
+    internal class MoexParser : IParser<string>
+    {
+        public MoexParser()
+        {
+
+        }
+        public List<string> Load()
+        {
+            SqlCrud sql=new SqlCrud();
+            
+            List<string> moexCompanies
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.moex.com/");
+            
+        }
+
+        public void Send()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
