299ea47a89dbcfed237e0dc005eee5f9b909c794
koeno <t-7892009@mail.ru>
2022-07-08 15:08:25 +0300

    Parsed price + time MOEX

---
+++ ParserServ/MOEX.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+
+namespace ParserServ;
+
+public class MOEX
+{
+    public void downloadXML(string board, string secid)
+    {
+        var xml = XDocument.Load(
+            $@"https://iss.moex.com/iss/engines/stock/markets/shares/boards/{board}/securities/{secid}/.xml?iss.meta=off");
+
+        var last_price = xml.Elements("document").Elements("data")
+            .FirstOrDefault(x => x.Attribute("id")?.Value == "marketdata")!
+            .Element("rows")
+            ?.Element("row")!.Attribute("LAST");
+
+        var time_moex = xml.Elements("document").Elements("data")
+            .FirstOrDefault(x => x.Attribute("id")?.Value == "marketdata")!
+            .Element("rows")
+            ?.Element("row")!.Attribute("TIME");
+
+        Console.ReadKey();
+    }
+}
