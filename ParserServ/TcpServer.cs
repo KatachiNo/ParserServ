c98044b7bc58ea1f05e4676f54811fc80b1d8972
koeno <t-7892009@mail.ru>
2022-07-09 12:15:20 +0300

    Alpha Tcp Server

---
+++ ParserServ/TcpServer.cs
@@ -0,0 +1,40 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ParserServ;
+
+public class TcpServer
+{
+    private TcpListener server;
+
+
+    private TcpServer(string ip, int port)
+    {
+        var localAddress = IPAddress.Parse(ip);
+
+        server = new TcpListener(localAddress, port);
+        server.Start();
+    }
+
+    public void TcpServerReading()
+    {
+        try
+        {
+            while (true)
+            {
+                var client = server.AcceptTcpClient();
+                var stream = client.GetStream();
+                Console.WriteLine("Подключен клиент.");
+
+                var data = new byte[256];
+                var bytes = stream.Read(data, 0, data.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(data, 0, bytes));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
