37447473326cef48ef7a7646c51ab1f6f0ec875f
koeno <t-7892009@mail.ru>
2022-07-10 12:19:46 +0300

    alpha v0.2

--- ParserServ/MOEX.cs
+++ ParserServ/MOEX.cs
@@ -1,11 +1,22 @@
-﻿using System.Xml.Linq;
-
+﻿using System.Data.SqlClient;
+using System.Xml.Linq;
 
 namespace ParserServ;
 
 public class Moex
 {
-    public (string, string, string, string) DownloadXml(string board, string secid)
+    public void AddStock(string secID, string board)
+    {
+        var connection =
+            new SqlConnection(
+                @"Server=sql.bsite.net\MSSQL2016;Persist Security Info=True;User ID=metallplaceproject_SampleDB;Password=12345");
+        connection.Open();
+        var command = new SqlCommand($@"INSERT INTO Moex (SecID,Board) VALUES ('{secID}','{board}')",connection).ExecuteNonQuery();
+        connection.Close();
+    }
+
+
+    public (string, string, string, string) TakeData(string board, string secid)
     {
         var xml = XDocument.Load(
             $@"https://iss.moex.com/iss/engines/stock/markets/shares/boards/{board}/securities/{secid}/.xml?iss.meta=off");
@@ -15,15 +26,22 @@ public class Moex
             .Element("rows")
             ?.Element("row")!.Attribute("LAST");
 
-        var timeMoex = xml.Elements("document").Elements("data")
+        var timeMoex_temp = xml.Elements("document").Elements("data")
             .FirstOrDefault(x => x.Attribute("id")?.Value == "marketdata")!
             .Element("rows")
-            ?.Element("row")!.Attribute("TIME");
+            ?.Element("row")!.Attribute("TIME").Value;
 
+        var prevdate = xml.Elements("document").Elements("data")
+            .FirstOrDefault(x => x.Attribute("id")?.Value == "securities")!
+            .Element("rows")
+            ?.Element("row")!.Attribute("PREVDATE").Value.ToString();
+
+        var t = DateTime.Parse(timeMoex_temp);
+        var fullData = DateTime.Parse(prevdate).AddDays(1).Add(new TimeSpan(t.Hour, t.Minute, t.Second)).ToString();
 
         Console.WriteLine(
-            $"Акция {secid} последняя цена {lastPrice.Value} время парсинга {DateTime.Now} Время по MOEX {timeMoex}");
+            $"Акция {secid} последняя цена {lastPrice.Value} время парсинга {DateTime.Now} Время по MOEX {fullData}");
 
-        return (secid, lastPrice.Value, DateTime.Now.ToString(), timeMoex.ToString());
+        return (secid, DateTime.Now.ToString(), fullData, lastPrice.Value);
     }
 }
--- ParserServ/Program.cs
+++ ParserServ/Program.cs
@@ -1,66 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using ParserServ;
 
 var moex = new Moex();
 
+var connection =
+    new SqlConnection(
+        @"Server=sql.bsite.net\MSSQL2016;Persist Security Info=True;User ID=metallplaceproject_SampleDB;Password=12345");
+await connection.OpenAsync();
 
 
 
-// using (SqlConnection connection = new SqlConnection(connectionString))
-// {
-//     await connection.OpenAsync();
-//     var command = new SqlCommand();
-//     command.CommandText = @"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-//                           VALUES (1,'2007-05-08 12:35:29.1234567','2007-05-08 12:35:29.1234567',32123)";
-//     command.Connection = connection;
-// }
 
 
-var connectionString =
-    @"sql.bsite.net\MSSQL2016;Persist Security Info=True;User ID=metallplaceproject_SampleDB;Password=12345;";
-var connection = new SqlConnection(connectionString);
-connection.Open();
-// await connection.OpenAsync();
-// var command = new SqlCommand();
-// command.CommandText = @"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-//                           VALUES (1,'2007-05-08 12:35:29.1234567','2007-05-08 12:35:29.1234567',32123)";
-// command.Connection = connection;
+
 
 while (true)
-{   
-    var a = moex.DownloadXml("TQBR", "AMEZ");
-    var b = moex.DownloadXml("TQBR", "IGST");
-    var c = moex.DownloadXml("TQBR", "IGSTP");
-    var d = moex.DownloadXml("TQBR", "KMEZ");
-        
-    
-    await connection.OpenAsync();
+{
+    var a = moex.TakeData("TQBR", "AMEZ");
+    var b = moex.TakeData("TQBR", "IGST");
+    var c = moex.TakeData("TQBR", "IGSTP");
+    var d = moex.TakeData("TQBR", "KMEZ");
+
+
     var commanda = new SqlCommand();
     var commandb = new SqlCommand();
     var commandc = new SqlCommand();
     var commandd = new SqlCommand();
-   
-    
-    commanda.CommandText = $@"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-                          VALUES (1,'{a.Item2}','{a.Item3}',{a.Item4})";
-    
-    commandb.CommandText = $@"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-                          VALUES (4,'{b.Item2}','{b.Item3}',{b.Item4})";
-    
-    commandc.CommandText = $@"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-                          VALUES (3,'{c.Item2}','{c.Item3}',{c.Item4})";
-    
-    commandd.CommandText = $@"INSERT INTO MoexDataAll (SecID, ParsingDate, DataMOEX, LastPrice) 
-                          VALUES (5,'{d.Item2}','{d.Item3}',{d.Item4})";
-    
+
+
+    commanda.CommandText =
+        $@"INSERT INTO MoexDataAll (SecIDNum, ParsingDate, DataMOEX, LastPrice) VALUES (1,'{a.Item2}','{a.Item3}',{a.Item4})";
+
+    commandb.CommandText =
+        $@"INSERT INTO MoexDataAll (SecIDNum, ParsingDate, DataMOEX, LastPrice) VALUES (4,'{b.Item2}','{b.Item3}',{b.Item4})";
+
+    commandc.CommandText =
+        $@"INSERT INTO MoexDataAll (SecIDNum, ParsingDate, DataMOEX, LastPrice) VALUES (3,'{c.Item2}','{c.Item3}',{c.Item4})";
+
+    commandd.CommandText =
+        $@"INSERT INTO MoexDataAll (SecIDNum, ParsingDate, DataMOEX, LastPrice) VALUES (5,'{d.Item2}','{d.Item3}',{d.Item4})";
+
     commanda.Connection = connection;
     commandb.Connection = connection;
     commandc.Connection = connection;
     commandd.Connection = connection;
-    
-    
-    
+    commanda.ExecuteNonQuery();
+    commandb.ExecuteNonQuery();
+    commandc.ExecuteNonQuery();
+    commandd.ExecuteNonQuery();
+
+    Console.WriteLine("Sleeping. . .");
     Thread.Sleep(60000); // 1 min = 60000 ms
+    
 }
+
