d8407487a5e864fc40fd508772e8aae1b794b135
Наталия <m1908592@edu.misis.ru>
2022-07-11 22:29:31 +0300

    Коммит (парсер без бд)

--- ParserServ/mcena.cs
+++ ParserServ/mcena.cs
@@ -17,17 +17,18 @@ public class McenaParser
         urls.Add(new string[] { "4", "https://www.mcena.ru/metalloprokat/list" });
         urls.Add(new string[] { "5", "https://www.mcena.ru/metalloprokat/list" });
         int id = 1;
-        foreach (string[] url in urls)
+        List<Thread> stream = new List<Thread>();
+        foreach (string[] u in urls)
         {
-            Pars(url[1], Int32.Parse(url[0]), id);
+            Pars(u[1], Int32.Parse(u[0]), id);
             id++;
         }
     }
-    public void Pars(string url, int line, int id)
+    public static void Pars(string url, int line, int id)
     {
         ChromeOptions option = new ChromeOptions();
         option.AddArgument("headless");
-        IWebDriver driver = new ChromeDriver();
+        IWebDriver driver = new ChromeDriver(option);
         driver.Navigate().GoToUrl(url);
         string q = "body > main > div.main__content > article:nth-child(3) > div > div > div > div > table > tbody > tr:nth-child(" + line.ToString() + ") > td:nth-child(2)";
         var v = driver.FindElement(By.CssSelector(q));
