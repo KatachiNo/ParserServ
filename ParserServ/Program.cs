2935b1c688a77b7f495092ea4abf789534dab14e
koeno <t-7892009@mail.ru>
2022-07-12 22:35:56 +0300

    Объеденил ветку Moex и Tcp_Server_Part

--- ParserServ/Program.cs
+++ ParserServ/Program.cs
@@ -1,4 +1,27 @@
-using ParserServ.InternetParser;
+using System.Net.Sockets;
+using ParserServ;
 
-MoexParser moexParser = new MoexParser();
-moexParser.Load();
+var port = 8080;
+
+var listener = new TcpListener(port);
+try
+{
+    listener.Start();
+    Console.WriteLine("Waiting connections...");
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        // создаем новый поток для обслуживания нового клиента
+        new Thread(() => { new TcpServer(client).Process(); }).Start();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    if (listener != null)
+        listener.Stop();
+}
