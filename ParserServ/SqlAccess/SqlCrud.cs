d5037fd4ac13ea960d8451ec19d6628e55c210fe
Никита Зимин <m1900757@edu.misis.ru>
2022-07-18 11:52:08 +0300

    изменил запрос под новую таблицу

--- ParserServ/SqlAccess/SqlCrud.cs
+++ ParserServ/SqlAccess/SqlCrud.cs
@@ -16,16 +16,14 @@ namespace ParserServ.SqlAccess
             _dataAccess = new SqliteDataAccess();
             _connectionString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
         }
-        public List<string> GetMoexCompanies()
-        {
-            string sql="Select CompanyName From MoexCompany";
-            return _dataAccess.LoadData<string, dynamic>(sql, new { }, _connectionString);
-        }
+       
         public void AddToEconomics(string name,string price)
         {
             DateTime date = DateTime.Now;
-            string sql = " insert into TradingeconomicsTable values(@name,@price,@date);";
-            _dataAccess.SaveData(sql, new {name,price,date},_connectionString);
+            string sql = " IF NOT EXISTS ( select Id From EconomicsTable Where ProductName=@name) begin insert into EconomicsTable values(@name) end";
+            _dataAccess.SaveData(sql, new {name},_connectionString);
+            sql = "insert into EconomicsParse values((select Id from EconomicsTable where ProductName=@name),@price,@date)";
+            _dataAccess.SaveData(sql, new { name,price,date }, _connectionString);
         }
     }
 }
